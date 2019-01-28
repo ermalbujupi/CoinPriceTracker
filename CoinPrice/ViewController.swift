6d3966ca8dd7a449ce1701247e0b67e9aa046f87
Ermal <ermal.bujupaj@openresearch.com>
2019-01-28 14:30:54 +0100

    First Commit

---
+++ CoinPrice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoinPrice
+//
+//  Created by Ermal Buju on 28.01.19.
+//  Copyright © 2019 Ermal Bujupaj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
