e2b6f067be79575433a084b313fd96b957de532b
Ermal <ermal.bujupaj@openresearch.com>
2019-01-28 14:55:24 +0100

    Add main view with UI elements

--- CoinPrice/ViewController.swift
+++ CoinPrice/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  CoinPrice
 //
-//  Created by Ermal Buju on 28.01.19.
+//  Created by Ermal Bujupaj on 28.01.19.
 //  Copyright © 2019 Ermal Bujupaj. All rights reserved.
 //
 
@@ -10,9 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var mainView: CoinPriceMainView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        mainView = CoinPriceMainView(frame: CGRect.zero)
+        self.view.addSubview(mainView)
+        
+        setupConstraints()
+    }
+    
+    private func setupConstraints() {
+        mainView.translatesAutoresizingMaskIntoConstraints = false
+        mainView.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
+        mainView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor).isActive = true
+        mainView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor).isActive = true
+        mainView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor).isActive = true
     }
 
 
